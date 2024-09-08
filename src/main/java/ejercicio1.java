a0f475106007090206ce5e37545d275252988ccf
Johstin <johstiin@gmail.com>
2024-09-08 16:35:58 -0500

    primer ejercicio

---
+++ src/main/java/ejercicio1.java
@@ -0,0 +1,17 @@
+package src.main.java;
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entero: ");
+        int n = scanner.nextInt();
+
+        int suma = 0;
+        for (int i = 2; i <= n; i += 2) {
+            suma += i;
+        }
+
+        System.out.println("La suma de los números pares entre 1 y " + n + " es: " + suma);
+    }
+}
